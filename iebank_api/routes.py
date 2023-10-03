4ec01bf0a1c6e07fe2e9d3c70604d77afdab43bf
Chiara Di Giannantonio <chiaradigi2@gmail.com>
2023-10-03 12:04:15 +0200

    add country field

--- iebank_api/routes.py
+++ iebank_api/routes.py
@@ -64,5 +64,6 @@ def format_account(account):
         'balance': account.balance,
         'currency': account.currency,
         'status': account.status,
-        'created_at': account.created_at
+        'country': account.country,
+        'created_at': account.created_at, 
     }
